4dfe55f949b6f08faf7479850877b18aa51dccda
AlexMl <alexandermlinzk@gmail.com>
2015-10-19 15:46:37 +0200

    Added plugin logger

--- src/me/Aubli/RankCommands/RankCommands.java
+++ src/me/Aubli/RankCommands/RankCommands.java
@@ -1,24 +1,54 @@
 package me.Aubli.RankCommands;
 
+import java.util.logging.Level;
+
+import me.Aubli.Util.Logger.PluginOutput;
+
 import org.bukkit.plugin.java.JavaPlugin;
 
 
 public class RankCommands extends JavaPlugin {
     
     private static RankCommands instance;
+    private static PluginOutput logger;
+    
+    private boolean debugMode;
+    private int logLevel;
     
     @Override
     public void onDisable() {
-	
+	logger.log(getClass(), "Plugin disabled!", false);
     }
     
     @Override
     public void onEnable() {
 	instance = this;
+	
+	loadPluginConfig();
+	
+	logger = new PluginOutput(getInstance(), this.debugMode, this.logLevel);
+	
+	logger.log(getClass(), "Plugin enabled!", false);
     }
     
     public static RankCommands getInstance() {
 	return instance;
+    }
+    
+    public static PluginOutput getPluginLogger() {
+	return logger;
+    }
+    
+    private void loadPluginConfig() {
+	
+	getConfig().addDefault("plugin.debugMode", false);
+	getConfig().addDefault("plugin.logLevel", Level.INFO.intValue());
+	
+	this.debugMode = getConfig().getBoolean("plugin.debugMode");
+	this.logLevel = getConfig().getInt("plugin.logLevel");
+	
+	getConfig().options().copyDefaults(true);
+	saveConfig();
     }
     
 }
---
+++ src/me/Aubli/Util/Logger/PluginOutput.java
@@ -0,0 +1,126 @@
+package me.Aubli.Util.Logger;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+
+public class PluginOutput {
+    
+    private final Logger log;
+    
+    private File logFile;
+    
+    private boolean debugMode;
+    private int loglevel;
+    
+    private String pluginVersion;
+    
+    public PluginOutput(JavaPlugin plugin, boolean enableDebug, int minLevel) {
+	
+	this.log = plugin.getLogger();
+	
+	this.pluginVersion = plugin.getDescription().getVersion();
+	
+	this.debugMode = enableDebug;
+	
+	this.loglevel = minLevel;
+	
+	this.logFile = new File(plugin.getDataFolder().getAbsolutePath() + "/logs/pluginlog-" + this.pluginVersion + ".info");
+	
+	try {
+	    this.logFile.getParentFile().mkdirs();
+	    this.logFile.createNewFile();
+	} catch (IOException e) {
+	    e.printStackTrace();
+	}
+	
+    }
+    
+    public int getLogLevel() {
+	return this.loglevel;
+    }
+    
+    public boolean isDebugMode() {
+	return this.debugMode;
+    }
+    
+    public void log(Class<?> senderClass, String message, boolean logMessage) {
+	log(senderClass, Level.INFO, message, logMessage, false);
+    }
+    
+    public void log(Class<?> senderClass, Level level, String message, boolean debugMessage) {
+	log(senderClass, level, message, true, debugMessage, null);
+    }
+    
+    public void log(Class<?> senderClass, Level level, String message, boolean logMessage, boolean debugMessage) {
+	log(senderClass, level, message, logMessage, debugMessage, null);
+    }
+    
+    public void log(Class<?> senderClass, Level level, String message, boolean logMessage, boolean debugMessage, Exception e) {
+	
+	if (logMessage || e != null || level.intValue() >= Level.WARNING.intValue()) {
+	    logData(senderClass, level, message, e);
+	}
+	
+	if (e != null) {
+	    this.log.log(Level.WARNING, message);
+	    this.log.log(Level.WARNING, e.toString());
+	    return;
+	}
+	
+	if (!debugMessage) {
+	    if (level.intValue() >= Level.INFO.intValue()) {
+		this.log.log(level, message);
+		return;
+	    }
+	    this.log.info("[" + level.getName() + "] " + message);
+	    return;
+	}
+	
+	if (debugMessage) {
+	    if (this.debugMode && level.intValue() >= getLogLevel()) {
+		if (level.intValue() >= Level.INFO.intValue()) {
+		    this.log.log(level, message);
+		    return;
+		}
+		this.log.info("[" + level.getName() + "] " + message);
+		return;
+	    }
+	}
+    }
+    
+    private void logData(Class<?> senderClass, Level level, String message, Exception exception) {
+	SimpleDateFormat formatter = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss ('UTC' Z)");
+	Date currentTime = new Date();
+	
+	try {
+	    FileWriter writer = new FileWriter(this.logFile, true);
+	    
+	    if (exception != null) {
+		StringWriter sw = new StringWriter();
+		PrintWriter pw = new PrintWriter(sw);
+		exception.printStackTrace(pw);
+		
+		message += "\n" + sw.toString();
+	    }
+	    
+	    writer.write("[" + formatter.format(currentTime) + "] [" + senderClass.getSimpleName() + "] [" + level.getName() + "] " + message);
+	    writer.write(System.getProperty("line.separator"));
+	    writer.flush();
+	    writer.close();
+	    
+	} catch (IOException e) {
+	    e.printStackTrace();
+	}
+    }
+    
+}
