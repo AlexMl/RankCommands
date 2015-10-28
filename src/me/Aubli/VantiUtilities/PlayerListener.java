7fddedc0fb537e80e6ab2ac8d131c2dcbe95bd87
AlexMl <alexandermlinzk@gmail.com>
2015-10-28 14:36:31 +0100

    Readd nightvision effect on respawn

--- src/me/Aubli/VantiUtilities/PlayerListener.java
+++ src/me/Aubli/VantiUtilities/PlayerListener.java
@@ -1,7 +1,12 @@
 package me.Aubli.VantiUtilities;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 import me.Aubli.VantiUtilities.Regenerator.RegenerationType;
 
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -9,9 +14,14 @@ import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.FoodLevelChangeEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 
 public class PlayerListener implements Listener {
+    
+    private List<UUID> nightVisionEnabled = new ArrayList<UUID>();
     
     @EventHandler
     public void onFoodLevelChange(FoodLevelChangeEvent event) {
@@ -61,6 +71,30 @@ public class PlayerListener implements Listener {
 	    event.setKeepInventory(true);
 	} else {
 	    event.setKeepInventory(false);
+	}
+	
+	if (event.getEntity().hasPotionEffect(PotionEffectType.NIGHT_VISION)) {
+	    if (VantiPermission.hasPermission(event.getEntity(), VantiPermission.player_nightvision)) {
+		this.nightVisionEnabled.add(event.getEntity().getUniqueId());
+	    }
+	}
+    }
+    
+    @EventHandler
+    public void onPlayerRespawn(PlayerRespawnEvent event) {
+	if (this.nightVisionEnabled.contains(event.getPlayer().getUniqueId())) {
+	    if (VantiPermission.hasPermission(event.getPlayer(), VantiPermission.player_nightvision)) {
+		final Player eventPlayer = event.getPlayer();
+		
+		Bukkit.getScheduler().runTask(VantiUtilities.getInstance(), new Runnable() {
+		    
+		    @Override
+		    public void run() {
+			PlayerListener.this.nightVisionEnabled.remove(eventPlayer.getPlayer().getUniqueId());
+			eventPlayer.getPlayer().addPotionEffect(new PotionEffect(PotionEffectType.NIGHT_VISION, Integer.MAX_VALUE, 1), true);
+		    }
+		});
+	    }
 	}
     }
 }
